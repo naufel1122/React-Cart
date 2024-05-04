81685eff4b7f580580a42c24df15de9606faeca6
naufel1122 <nofil.khan037@gmail.com>
2024-05-04 14:52:48 +0500

    working

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar.JSX";
 
 function App() {
   return 
   <>
-  
+  <Navbar/>
   </>;
 }
 
