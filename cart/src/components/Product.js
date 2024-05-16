c696c1b11c3ceb63896c6082c05a45aef0e96320
naufel1122 <nofil.khan037@gmail.com>
2024-05-16 17:01:01 +0500

    Button

--- cart/src/components/Product.js
+++ cart/src/components/Product.js
@@ -3,9 +3,19 @@ import React from 'react'
 export default function Product(props) {
   return (
     <div className='row' >
-        <div className='col-6'>
-            <h2>{props.product.name}<span class="badge bg-secondary">₨={props.product.Price}</span></h2>
+      <div className='col-5'>
+        <h2>{props.product.name}<span class="badge bg-secondary">₨={props.product.Price}</span></h2>
+      </div>
+      <div className='col-3' >
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+          <button type="button" class="btn btn-danger">-</button>
+          <button type="button" class="btn btn-warning">{props.product.quantity}</button>
+          <button type="button" class="btn btn-success">+</button>
         </div>
+      </div>
+      <div className='col-4'>
+        {props.product.quantity*props.product.price}
+      </div>
     </div>
   )
 }
