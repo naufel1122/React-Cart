be178cfe80009ac229114b63db1cdfdd676630c1
naufel1122 <nofil.khan037@gmail.com>
2024-05-23 22:16:28 +0500

    working on total

--- cart/src/App.js
+++ cart/src/App.js
@@ -12,39 +12,49 @@ import React, { useState } from 'react';
 function App() {
   const products = [
     {
-      price: 900000,
+      price: 100000,
       name: 'Iphone 15 Pro Max',
       quantity: 0,
     },
     {
-      price: 1000000,
+      price: 300000,
       name: 'Samsung S24 Ultra',
       quantity: 0,
     }
   ];
 
-  let  [productList, setProductList] = useState(products);
+  let [productList, setProductList] = useState(products);
+  let [totalAmount, setTotalAmount] = useState(0);
 
   const incrementQuantity = (index) => {
     let newProductList = [...productList];
+    let newTotalAmount = [totalAmount];
     newProductList[index].quantity++;
+    newTotalAmount += newProductList[index].price
+    setTotalAmount(newTotalAmount)
     setProductList(newProductList);
   };
   const decrementQuantity = (index) => {
     let newProductList = [...productList];
-    newProductList[index].quantity > 0 ? newProductList[index].quantity-- : newProductList[index].quantity = 0
+    let newTotalAmount = [totalAmount];
+    if (newProductList[index].quantity > 0) {
+      newProductList[index].quantity--
+      newTotalAmount -= newProductList[index].price
+    }
+    //  : newProductList[index].quantity = 0
+    setTotalAmount(newTotalAmount);
     setProductList(newProductList);
-  };
+};
 
-  return (
-    <>
-      <Navbar />
-      <main className="container mt-5">
-        <ProductList productList={productList} incrementQuantity={incrementQuantity} decrementQuantity={decrementQuantity}/>
-      </main>
-      <Footer />
-    </>
-  );
+return (
+  <>
+    <Navbar />
+    <main className="container mt-5">
+      <ProductList productList={productList} incrementQuantity={incrementQuantity} decrementQuantity={decrementQuantity} />
+    </main>
+    <Footer totalAmount={totalAmount} />
+  </>
+);
 }
 
 export default App;
